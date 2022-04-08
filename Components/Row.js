7ba4dab5737f08204b1e3373d9da2797c4e73378
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 07:48:47 -0400

    add imports, component and styles in Row

---
+++ Components/Row.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import { useEffect } from 'react/cjs/react.production.min';
+import Square from './Square';
+
+const Row = (props) => {
+
+}
+
+const rowStyles = StyleSheet.create ({
+    main: {
+        flex: 1,
+        flexDirection: 'row',
+        marginVertical: 5
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    }
+
+})
+
+export default Row;
