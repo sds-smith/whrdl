9aeac4b6e0a9258bdf5be970b6aad86b3d142f0c
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 07:17:12 -0400

    add state in App

--- App.js
+++ App.js
@@ -6,10 +6,22 @@ import Header from './Components/Header'
 import Board from './Components/Board'
 import Keyboard from './Components/Keyboard'
 
-
 export default function App() {
+  const board = {
+    guess1 : '',
+    guess2 : '',
+    guess3 : '',
+    guess4 : '',
+    guess5 : '',
+    guess6 : ''
+  }
 
-  
+  const [guessState, setGuessState] = useState(board)  
+  const [currentGuess, setCurrentGuess] = useState('guess1')
+  const [currentLetter,setCurrentLetter] = useState(0)
+  const [targetWord, setTargetWord] = useState('orate')
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
