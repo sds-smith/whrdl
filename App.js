22d250bb677392efb618b6b84585973aa6305ae6
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 07:16:11 -0400

    add imports in App

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import * as React from 'react'
+import { useState } from 'react'
+import Header from './Components/Header'
+import Board from './Components/Board'
+import Keyboard from './Components/Keyboard'
+
 
 export default function App() {
+
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
