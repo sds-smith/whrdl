dcecc5b6988ee68a2c433607725b6981ba06919c
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 13:36:10 -0400

    remove all conditional logic from Square

--- Components/Square.js
+++ Components/Square.js
@@ -5,46 +5,26 @@ import { StyleSheet, View, Text, TextInput } from 'react-native';
 const Square = (props) => {
     const {guessId, index, guessState, currentGuess, currentLetter, onLetterEntry} = props
     const [value, setValue] = useState('')
-    const [render, setRender] = useState(true)
     
     const handleEntry = (e) => {
         setValue(e.target.value)
-        
         onLetterEntry(e.target.value)
     }
 
     let autofocus = ((currentGuess === guessId) && (currentLetter === index))
-    const textInput = <TextInput 
-                        value={value}
-                        guessState={guessState}
-                        currentGuess={currentGuess}
-                        style={squareStyles.main}
-                        autoCapitalize='characters'
-                        caretHidden
-                        maxLength={1}
-                        onChange={handleEntry}
-                        autoFocus={autofocus}
-                      />
-    let altSquare = <Text style={squareStyles.alts}>HALLO</Text>
-    let component = textInput
-
-    if ((currentLetter === index) && (!render)) {
-        component = null
-    }
-
-    useEffect(() => {
-        if (currentLetter !== 0) {
-            setRender(false)
-
-        }
-        return setRender(true)
-    }, [currentLetter])
 
     return (
-        <View>
-            {component}
-        </View>
-
+        <TextInput 
+            value={value}
+            guessState={guessState}
+            currentGuess={currentGuess}
+            style={squareStyles.main}
+            autoCapitalize='characters'
+            caretHidden
+            maxLength={1}
+            onChange={handleEntry}
+            autoFocus={autofocus}
+        />
     )
 }
 
@@ -55,13 +35,6 @@ const squareStyles = StyleSheet.create ({
         borderColor: 'gray',
         borderWidth: 1,
         textAlign: 'center'
-    },
-    alts: {
-        flex: 1,
-        marginHorizontal: 1,
-        borderColor: 'salmon',
-        borderWidth: 1,
-        textAlign: 'center',
     }
 })
 
