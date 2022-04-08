a3e32381094ab69038cf3eb163fa96a7f99a9fce
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 13:45:31 -0400

    add useRef in row

--- Components/Row.js
+++ Components/Row.js
@@ -1,43 +1,44 @@
 import * as React from 'react';
+import { useRef } from 'react'
 import { StyleSheet, View, Button } from 'react-native';
-import { useState } from 'react/cjs/react.production.min';
 import Square from './Square';
 
 const Row = (props) => {
 
     const {guessId, guessState, currentGuess, currentLetter, onLetterEntry, onWordEntry} = props
+    const {square1, square2, square3, square4, square0} = useRef()
 
     return (
         <View style={rowStyles.main}>
-            <Square index={0} id={'0'}
+            <Square index={0} id={'0'} ref={square0}
                 guessId={guessId} 
                 guessState={guessState}
                 currentGuess={currentGuess}
                 currentLetter={currentLetter} 
                 onLetterEntry={onLetterEntry}
             />
-            <Square index={1} id={'1'} 
+            <Square index={1} id={'1'} ref={square1} 
                 guessId={guessId}
                 guessState={guessState}
                 currentGuess={currentGuess}
                 currentLetter={currentLetter} 
                 onLetterEntry={onLetterEntry}
             />
-            <Square index={2} id={'2'}
+            <Square index={2} id={'2'} ref={square2}
                 guessId={guessId} 
                 guessState={guessState}
                 currentGuess={currentGuess}
                 currentLetter={currentLetter} 
                 onLetterEntry={onLetterEntry}
             />
-            <Square index={3} id={'3'} 
+            <Square index={3} id={'3'}  ref={square3}
                 guessId={guessId}
                 guessState={guessState}
                 currentGuess={currentGuess}
                 currentLetter={currentLetter} 
                 onLetterEntry={onLetterEntry}
             />
-            <Square index={4} id={'4'}
+            <Square index={4} id={'4'} ref={square4}
                 guessId={guessId} 
                 guessState={guessState}
                 currentGuess={currentGuess}
