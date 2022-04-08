3b26cf0a73ade6d432a60d02cc254d45473ec3f6
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 07:43:42 -0400

    add imports, component, and styles in Header

---
+++ Components/Header.js
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Header = () => {
+    return (
+        <View style={headerStyles.main}>
+            <Text style={headerStyles.text}>WHRDL</Text>
+        </View>
+    )
+}
+
+const headerStyles = StyleSheet.create ({
+    main: {
+        flex: 1,
+        marginHorizontal: 5,
+        width: '100%',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 5
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: '500'
+    }
+
+})
+
+export default Header;
