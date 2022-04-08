9eeb9fe7a4e1a0b5da64cd96954e5a8368e9bded
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 07:45:48 -0400

    add imports, component and styles in Board

---
+++ Components/Board.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import  Row from './Row';
+
+const Board = (props) => {
+
+}
+
+const boardStyles = StyleSheet.create ({
+    main: {
+        flex: 6,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        width: '90%',
+        // backgroundColor: 'gray'
+    }
+
+})
+
+export default Board;
