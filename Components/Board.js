3ace2e457811c3785a4068d9928d0866d7665d3d
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 07:47:21 -0400

    add return statement in Board

--- Components/Board.js
+++ Components/Board.js
@@ -6,6 +6,52 @@ const Board = (props) => {
 
     const {guessState, currentGuess, currentLetter, handleLetterEntry, handleWordEntry} = props
 
+    return (
+        <View style={boardStyles.main}>
+            <Row id={1} guessId={'guess1'} 
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter}
+                onLetterEntry={(letter)=> handleLetterEntry(letter)} 
+                onWordEntry={handleWordEntry}
+            />
+            <Row id={2} guessId={'guess2'}
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter}
+                onLetterEntry={(letter)=> handleLetterEntry(letter)} 
+                onWordEntry={handleWordEntry}
+            />
+            <Row id={3} guessId={'guess3'}
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter}
+                onLetterEntry={(letter)=> handleLetterEntry(letter)} 
+                onWordEntry={handleWordEntry}
+            />
+            <Row id={4} guessId={'guess4'}
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter}
+                onLetterEntry={(letter)=> handleLetterEntry(letter)} 
+                onWordEntry={handleWordEntry}
+            />
+            <Row id={5} guessId={'guess5'}
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter}
+                onLetterEntry={(letter)=> handleLetterEntry(letter)} 
+                onWordEntry={handleWordEntry}
+            />  
+            <Row id={6} guessId={'guess6'}
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter}
+                onLetterEntry={(letter)=> handleLetterEntry(letter)} 
+                onWordEntry={handleWordEntry}
+            />  
+        </View>
+    )
 }
 
 const boardStyles = StyleSheet.create ({
