c1406ccd7d3ce404d96650b8047932bf5935b6d8
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 07:50:12 -0400

    add return statement in Row

--- Components/Row.js
+++ Components/Row.js
@@ -7,6 +7,41 @@ const Row = (props) => {
 
     const {guessState, currentGuess, currentLetter, onLetterEntry, onWordEntry} = props
 
+    return (
+        <View style={rowStyles.main}>
+            <Square index={0} id={'0'} 
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter} 
+                onLetterEntry={onLetterEntry}
+            />
+            <Square index={1} id={'1'} 
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter} 
+                onLetterEntry={onLetterEntry}
+            />
+            <Square index={2} id={'2'} 
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter} 
+                onLetterEntry={onLetterEntry}
+            />
+            <Square index={3} id={'3'} 
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter} 
+                onLetterEntry={onLetterEntry}
+            />
+            <Square index={4} id={'4'} 
+                guessState={guessState}
+                currentGuess={currentGuess}
+                currentLetter={currentLetter} 
+                onLetterEntry={onLetterEntry}
+            />  
+            <Button title='ENTER' onPress={onWordEntry}/>          
+        </View>
+    )
 }
 
 const rowStyles = StyleSheet.create ({
