5f0ea2554e7a517664287c304283b059cdb4b2af
Shawn Smith <sds.smith24@gmail.com>
2022-04-08 07:51:35 -0400

    add imports, component and styles in Square

---
+++ Components/Square.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react'
+import { StyleSheet, TextInput } from 'react-native';
+
+const Square = (props) => {
+    
+}
+
+const squareStyles = StyleSheet.create ({
+    main: {
+        flex: 1,
+        marginHorizontal: 1,
+        borderColor: 'gray',
+        borderWidth: 1,
+        textAlign: 'center'
+    }
+
+})
+
+export default Square;
