fd7da065100730dabb0cdbc24e11d172bc22f4a5
Shawn Smith <sds.smith24@gmail.com>
2022-04-09 16:03:05 -0400

    add and style keys in Keyboard

--- Components/Key.js
+++ Components/Key.js
@@ -1,13 +1,31 @@
 import * as React from 'react';
-import { View, Button } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 
 const LetterKey = (props) => {
     const {title} = props
     return (
-        <View>
-          <Button title={title} /><br/>        
+        <View style={keyStyles.main}>
+          <TouchableOpacity style={keyStyles.button}><Text style={{fontWeight: 'bold', fontSize: 20}}>{title}</Text></TouchableOpacity>   
         </View>
     )
 }
+
+const keyStyles = StyleSheet.create({
+  main: {
+    height: '100%',
+    alignItems: 'center',
+    margin: 10,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10
+  },
+  button: {
+    paddingHorizontal: 20,
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  }
+})
 
 export default LetterKey
--- Components/Keyboard.js
+++ Components/Keyboard.js
@@ -1,45 +1,51 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import LetterKey from './Key.js'
 const Keyboard = () => {
 
     return (
         <View style={keyboardStyles.main}>
             <View style={keyboardStyles.row}>
-                <LetterKey style={keyboardStyles.letterButton} title='Q' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='W' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='E' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='R' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='T' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='Y' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='U' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='I' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='O' /><br/>
-                <LetterKey style={keyboardStyles.letterButton} title='P' /><br/>                
+                <LetterKey  title='Q' />
+                <LetterKey  title='W' />
+                <LetterKey  title='E' />
+                <LetterKey  title='R' />
+                <LetterKey  title='T' />
+                <LetterKey  title='Y' />
+                <LetterKey  title='U' />
+                <LetterKey  title='I' />
+                <LetterKey  title='O' />
+                <LetterKey  title='P' />               
             </View>
             <View style={keyboardStyles.row}>
-                <LetterKey style={keyboardStyles.letterButton} title='A' />
-                <LetterKey style={keyboardStyles.letterButton} title='S' />
-                <LetterKey style={keyboardStyles.letterButton} title='D' />
-                <LetterKey style={keyboardStyles.letterButton} title='F' />
-                <LetterKey style={keyboardStyles.letterButton} title='G' />
-                <LetterKey style={keyboardStyles.letterButton} title='H' />
-                <LetterKey style={keyboardStyles.letterButton} title='J' />
-                <LetterKey style={keyboardStyles.letterButton} title='K' />
-                <LetterKey style={keyboardStyles.letterButton} title='L' />
+                <LetterKey  title='A' />
+                <LetterKey  title='S' />
+                <LetterKey  title='D' />
+                <LetterKey  title='F' />
+                <LetterKey  title='G' />
+                <LetterKey  title='H' />
+                <LetterKey  title='J' />
+                <LetterKey  title='K' />
+                <LetterKey  title='L' />
             </View>
             <View style={keyboardStyles.row}>
-                <LetterKey style={keyboardStyles.funcButton} title='ENTER' />
-                <LetterKey style={keyboardStyles.letterButton} title='Z' />
-                <LetterKey style={keyboardStyles.letterButton} title='X' />
-                <LetterKey style={keyboardStyles.letterButton} title='C' />
-                <LetterKey style={keyboardStyles.letterButton} title='V' />
-                <LetterKey style={keyboardStyles.letterButton} title='B' />
-                <LetterKey style={keyboardStyles.letterButton} title='N' />
-                <LetterKey style={keyboardStyles.letterButton} title='M' />
-                <LetterKey style={keyboardStyles.funcButton} title='BACK' />
+                <LetterKey  title='ENTER' />
+                <LetterKey  title='Z' />
+                <LetterKey  title='X' />
+                <LetterKey  title='C' />
+                <LetterKey  title='V' />
+                <LetterKey  title='B' />
+                <LetterKey  title='N' />
+                <LetterKey  title='M' />
+                <FunctionKey title='&#9003;' />
             </View>
         </View>
+    )
+}
+
+const FunctionKey = (props) => {
+    return (
+        <TouchableOpacity style={keyboardStyles.funcButton}><Text style={{fontSize: 50}}>{props.title}</Text></TouchableOpacity>
     )
 }
 
@@ -58,18 +64,21 @@ const keyboardStyles = StyleSheet.create ({
         width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
-        borderWidth: 1,
-        marginVertical: 2
+        marginVertical: 5
     },
     letterButton: {
         flex: 1,
-        marginHorizontal: 5
     },
     funcButton: {
-        flex: 2,
-        marginHorizontal: 5
+        fontSize: 50,
+        paddingHorizontal: 20,
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,   
+        backgroundColor: '#f2f2f2'       
     }
-
 })
 
 export default Keyboard;
