d835a8e1a05be5caf42f83838a9b38497b9546b8
Shawn Smith <sds.smith24@gmail.com>
2022-04-10 09:20:10 -0400

    convert board object values to empty arrays

--- App.js
+++ App.js
@@ -7,39 +7,36 @@ import Board from './Components/Board'
 import Keyboard from './Components/Keyboard'
 
 export default function App() {
-  const board = {
-    guess1 : '',
-    guess2 : '',
-    guess3 : '',
-    guess4 : '',
-    guess5 : '',
-    guess6 : ''
-  }
+  const board = {}
   const guesses = ['guess1', 'guess2', 'guess3', 'guess4', 'guess5', 'guess6']
+  guesses.forEach(guess => {
+    board[guess] = []
+  })
+
   const [guessState, setGuessState] = useState(board)  
-  const [currentGuess, setCurrentGuess] = useState('guess1')
+  const [currentGuess, setCurrentGuess] = useState(guesses[0])
   const [currentLetter,setCurrentLetter] = useState(0)
   const [targetWord, setTargetWord] = useState('orate'.toUpperCase())
 
   const handleLetterEntry = (letter) => {
 
-    switch (true) {
-      case currentGuess === 'guess1' : 
+    switch (currentGuess) {
+      case guesses[0] : 
         setGuessState(guessState => ({...guessState, guess1 : guessState.guess1 + letter}))
         break
-      case currentGuess === 'guess2' : 
+      case guesses[1] : 
         setGuessState(guessState => ({...guessState, guess2 : guessState.guess2 + letter}))
         break
-      case currentGuess === 'guess3' : 
+      case guesses[2] : 
         setGuessState(guessState => ({...guessState, guess3 : guessState.guess3 + letter}))
         break
-      case currentGuess === 'guess4' : 
+      case guesses[3] : 
         setGuessState(guessState => ({...guessState, guess4 : guessState.guess4 + letter}))
         break
-      case currentGuess === 'guess5' : 
+      case guesses[4] : 
         setGuessState(guessState => ({...guessState, guess5 : guessState.guess5 + letter}))
         break
-      case currentGuess === 'guess6' : 
+      case guesses[5] : 
         setGuessState(guessState => ({...guessState, guess6 : guessState.guess6 + letter}))
         break 
       default :
