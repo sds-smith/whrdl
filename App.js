bba62e7332c9fc2a6ea9e63d66e511111d991493
Shawn Smith <sds.smith24@gmail.com>
2022-04-10 20:54:25 -0400

    refactor event handlers

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Board from './Components/Board'
 import Keyboard from './Components/Keyboard'
 
 export default function App() {
+
   const board = {}
   const matches = {}
   const guesses = ['guess1', 'guess2', 'guess3', 'guess4', 'guess5', 'guess6']
@@ -29,93 +30,18 @@ export default function App() {
   const [targetWord, setTargetWord] = useState('orate'.toUpperCase())
 
   const handleLetterEntry = (letter) => {
-    const guessArray = []
-    switch (currentGuess) {
-      case guesses[0] : 
-        guessState.guess1.forEach(ltr => {
-          guessArray.push(ltr)
-        })
-        guessArray[currentLetter]=letter
-        setGuessState(guessState => ({...guessState, guess1 : guessArray}))
-        break
-      case guesses[1] : 
-        guessState.guess2.forEach(ltr => {
-          guessArray.push(ltr)
-        })
-        guessArray[currentLetter]=letter
-        setGuessState(guessState => ({...guessState, guess2 : guessArray}))
-        break
-      case guesses[2] : 
-        guessState.guess3.forEach(ltr => {
-          guessArray.push(ltr)
-        })
-        guessArray[currentLetter]=letter
-        setGuessState(guessState => ({...guessState, guess3 : guessArray}))
-        break
-      case guesses[3] : 
-        guessState.guess4.forEach(ltr => {
-          guessArray.push(ltr)
-        })
-        guessArray[currentLetter]=letter
-        setGuessState(guessState => ({...guessState, guess4 : guessArray}))
-        break
-      case guesses[4] : 
-        guessState.guess5.forEach(ltr => {
-          guessArray.push(ltr)
-        })
-        guessArray[currentLetter]=letter
-        setGuessState(guessState => ({...guessState, guess5 : guessArray}))
-        break
-      case guesses[5] :
-        guessState.guess6.forEach(ltr => {
-          guessArray.push(ltr)
-        })
-        guessArray[currentLetter]=letter 
-        setGuessState(guessState => ({...guessState, guess6 : guessArray}))
-        break 
-      default :
-        break   
-    }
+    const guessArray = guessState[currentGuess]
+    guessArray[currentLetter]=letter
+    setGuessState(guessState => ({...guessState, currentGuess : guessArray}))
     setCurrentLetter(currentLetter => currentLetter + 1)
   }
 
   const handleDelete = () => {
     setCurrentLetter(currentLetter => currentLetter - 1)
     let currentWord
-    switch (currentGuess) {
-      case 'guess1' : 
-        currentWord = guessState.guess1
-        currentWord.splice(currentLetter, 1, '')
-        setGuessState(guessState => ({...guessState, guess1 : currentWord}))
-        break
-      case 'guess2' : 
-        currentWord = guessState.guess2
-        currentWord.splice(currentLetter, 1, '')
-        setGuessState(guessState => ({...guessState, guess2 : currentWord}))
-        break
-      case 'guess3' : 
-        currentWord = guessState.guess3
-        currentWord.splice(currentLetter, 1, '')
-        setGuessState(guessState => ({...guessState, guess3 : currentWord}))
-        break
-      case 'guess4' : 
-        currentWord = guessState.guess4
-        currentWord.splice(currentLetter, 1, '')
-        setGuessState(guessState => ({...guessState, guess4 : currentWord}))
-        break
-      case 'guess5' : 
-        currentWord = guessState.guess5
-        currentWord.splice(currentLetter, 1, '')
-        setGuessState(guessState => ({...guessState, guess5 : currentWord}))
-      break
-      case 'guess6' : 
-        currentWord = guessState.guess6
-        currentWord.splice(currentLetter, 1, '')
-        setGuessState(guessState => ({...guessState, guess6 : currentWord}))
-        break 
-      default :
-        break   
-    }
+    currentWord = guessState[currentGuess]
+    currentWord.splice(currentLetter, 1, '')
+    setGuessState(guessState => ({...guessState, currentGuess : currentWord}))
   }
 
   const handleWordEntry = () => {
@@ -126,21 +52,28 @@ export default function App() {
     const currentWord = guessState[currentGuess]
     const currentMatches = letterMatches[currentGuess]
     for (let i = 0; i < currentWord.length; i ++) {
+      const keyMatch = currentWord[i]
       if (currentWord[i] === target[i]) {
         currentMatches[i] = '#0f0'
-        keyboardMatch[currentWord[i]] = '#0f0'
+        setKeyboardMatch(keyboardMatch => ({
+          ...keyboardMatch, keyMatch :'#0f0'
+        }))
         setLetterMatches((letterMatches) => ({
           ...letterMatches, currentGuess : currentMatches
         }))
       } else if (target.includes(currentWord[i])) {
         currentMatches[i] = '#ff0'
-        keyboardMatch[currentWord[i]] = '#ff0'
+        setKeyboardMatch(keyboardMatch => ({
+          ...keyboardMatch, keyMatch :'#ff0'
+        }))
         setLetterMatches((letterMatches) => ({
           ...letterMatches, currentGuess : currentMatches
         }))
       } else {
         currentMatches[i] = '#999'
-        keyboardMatch[currentWord[i]] = '#999'
+        setKeyboardMatch(keyboardMatch => ({
+          ...keyboardMatch, keyMatch :'#999'
+        }))        
         setLetterMatches((letterMatches) => ({
           ...letterMatches, currentGuess : currentMatches
         }))
